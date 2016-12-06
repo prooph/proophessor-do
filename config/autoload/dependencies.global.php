9b3176af9adace32ff563a1570b2956996272179
prolic <saschaprolic@googlemail.com>
2016-12-06 19:16:47 +0800

    update copyright in all php files

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of prooph/proophessor-do.
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Zend\Expressive\Application;
 use Zend\Expressive\Container\ApplicationFactory;
 use Zend\Expressive\Helper;
--- src/Container/Model/Todo/ReopenTodoHandlerFactory.php
+++ src/Container/Model/Todo/ReopenTodoHandlerFactory.php
@@ -1,12 +1,11 @@
 <?php
-/*
- * This file is part of prooph/proophessor.
- * (c) 2014-2015 prooph software GmbH <contact@prooph.de>
+/**
+ * This file is part of prooph/proophessor-do.
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * Date: 2/16/16
  */
 namespace Prooph\ProophessorDo\Container\Model\Todo;
 
