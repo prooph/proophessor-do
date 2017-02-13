104eb15d394255626c3d7a3bd055ddcb1e366570
Aleksei Akireikin <opexus@gmail.com>
2017-02-13 00:48:53 -0500

    strict type declaration

--- tests/Model/Todo/Command/AddReminderToTodoTest.php
+++ tests/Model/Todo/Command/AddReminderToTodoTest.php
@@ -8,6 +8,8 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace ProophTest\ProophessorDo\Model\Todo\Command;
 
 use Prooph\ProophessorDo\Model\Todo\Command\AddReminderToTodo;
